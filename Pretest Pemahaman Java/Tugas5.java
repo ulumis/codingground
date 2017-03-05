66369680b36f72683792ecd98fb25906155e2442
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:13:45 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas5.java
@@ -0,0 +1,17 @@
+public class Tugas5{
+    
+    public static void HitungMundur(int angka){
+        
+        for(int i = angka; i>0; i--){
+            System.out.println(i);
+        }
+    }
+    
+    public static void main(String []args){
+        
+        HitungMundur(10);
+    
+    }
+    
+    
+}
