b45dd03e4b2ec241069cc9128e27ec1c587fb846
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:12:33 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,10 @@
+public class Tugas2{
+    
+    public static void main(String []args){
+        String[] nama = {"Siti" , "Ulumis" , "Sifaiya"};
+        
+        System.out.println(" 1 : "+nama[0]);
+        System.out.println(" 2 : "+nama[1]);
+        System.out.println(" 3 : "+nama[2]);
+     }
+}
