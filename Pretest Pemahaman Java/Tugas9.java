0bf5b00732014a127fbac67b246c4fc6a482a22d
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:10:29 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas9.java
@@ -0,0 +1,16 @@
+public class Tugas9{
+    
+    String nama;
+    
+    public Tugas9(String nama){
+        
+        this.nama = nama;
+    }
+        
+        Tugas9 data = new Tugas9("Ulumis Sifaiya");
+        
+        
+    public static void main(String []args){
+        
+    }
+}
