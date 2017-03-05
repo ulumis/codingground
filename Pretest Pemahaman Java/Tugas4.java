6a4e8a1e81de3c8677d73ea2d251b724f1b514cd
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:11:07 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas4.java
@@ -0,0 +1,14 @@
+public class Tugas4{
+    
+    public static int Penjumlahan(int a, int b){
+        return a + b;
+    }
+    
+    public static void main(String []args){
+        
+        int jumlah = Penjumlahan(4, 3);
+        System.out.println(jumlah);
+        
+    }
+    
+}
