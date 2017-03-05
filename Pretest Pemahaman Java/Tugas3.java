355b7d4ac9bc43e13a188c9eca443c4b0d3e5918
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:08:59 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas3.java
@@ -0,0 +1,12 @@
+public class Tugas3{
+    
+    public static void TampilAngka(int angka){
+        
+        System.out.println("Angka yang anda masukkan adalah angka "+angka);
+    }
+    
+    public static void main(String []args){
+        
+        TampilAngka(3);
+    }
+}
