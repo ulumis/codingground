bc1acdb9f61844e54a9e8ccbec8cd39c2e2b7ec4
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:09:48 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas7.java
@@ -0,0 +1,59 @@
+public class Tugas7{
+    
+    public static void Angka(int angka){
+        
+        switch(angka){
+            case 1 : 
+                System.out.println("I");
+                break;
+                
+            case 2 : 
+                System.out.println("II");
+                break;
+                
+            case 3 : 
+                System.out.println("III");
+                break;
+                
+            case 4 : 
+                System.out.println("IV");
+                break;
+                
+            case 5 : 
+                System.out.println("V");
+                break;
+                
+            case 6 : 
+                System.out.println("VI");
+                break;
+                
+            case 7 : 
+                System.out.println("VII");
+                break;
+                
+            case 8 : 
+                System.out.println("VIII");
+                break;
+                
+            case 9 : 
+                System.out.println("IX");
+                break;
+                
+            case 10 : 
+                System.out.println("X");
+                break;
+            
+            default : 
+                System.out.println("Angka Salah");
+                break;
+        }
+    }
+    
+    public static void main(String []args){
+        
+        Angka(7);
+    
+    }
+    
+    
+}
