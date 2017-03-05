f5f20615119f74ee245537f8ba50613e1ebb2614
ulumis <ulumissifaiya0407@gmail.com>
2017-03-05 21:10:26 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Ulumis";
+        String kota = "Jepara";
+        System.out.println("Nama = "+nama+" . Kota "+kota);
+     }
+}
